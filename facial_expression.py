fc1d15046f28b00b785befb98186c9727e39828c
Lezin Galibert <lezin.galibert@softwarelab.no>
2022-11-21 09:45:11 +0100

    Reconnaissance d'expressions

--- facial_expression.py
+++ facial_expression.py
@@ -1,8 +1,19 @@
 import random
-from flask import Flask, Response, render_template, request
-import cv2
 
-expressions = ["angry", "sad", "happy", "surprised", "disgust", "neutral"]
+import cv2
+import numpy as np
+import torch
+from flask import Flask, Response, render_template, request
+from PIL import Image
+from skimage.transform import resize
+from torch.autograd import Variable
+
+from facial_expression_prediction import transforms
+from facial_expression_prediction.models.vgg import VGG
+import torch.nn.functional as F
+
+expressions = ['angry', 'disgust', 'fear',
+               'happy', 'sad', 'surprised', 'neutral']
 
 app = Flask(__name__)
 video = cv2.VideoCapture(0)
@@ -10,21 +21,68 @@ face_cascade = cv2.CascadeClassifier()
 face_cascade.load(cv2.samples.findFile(
     "static/haarcascade_frontalface_alt.xml"))
 
+net = VGG('VGG19')
+checkpoint = torch.load(
+    '/Users/lezingalibert/GitProjects/facial-expressions/facial_expression_prediction/FER2013_VGG19/base_model.t7', map_location=torch.device('cpu'))
+net.load_state_dict(checkpoint['net'])
+net.cpu()
+net.eval()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+cut_size = 44
+transform_test = transforms.Compose([
+    transforms.TenCrop(cut_size),
+    transforms.Lambda(lambda crops: torch.stack(
+        [transforms.ToTensor()(crop) for crop in crops])),
+])
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+
+def get_emotion(img):
+    with torch.no_grad():
+        gray = rgb2gray(img)
+        gray = resize(gray, (48, 48), mode='symmetric').astype(np.uint8)
+
+        img = gray[:, :, np.newaxis]
+
+        img = np.concatenate((img, img, img), axis=2)
+        img = Image.fromarray(img)
+        inputs = transform_test(img)
+
+        ncrops, c, h, w = np.shape(inputs)
+
+        inputs = inputs.view(-1, c, h, w)
+        inputs = inputs.cpu()
+        outputs = net(inputs)
+
+        outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
+
+        score = F.softmax(outputs_avg)
+        _, predicted = torch.max(outputs_avg.data, 0)
+
+    print(predicted)
+
+    return predicted
+
+
 def generate_face(video):
     while True:
-        emotion = random.randint(0, 5)
-        img_emotion = cv2.imread('static/' + expressions[emotion] + '.png')
-        img_height, img_width, _ = img_emotion.shape
 
         success, image = video.read()
         frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         frame_gray = cv2.equalizeHist(frame_gray)
+
+        pred = get_emotion(image)
+        img_emotion = cv2.imread('static/' + expressions[pred] + '.png')
+        img_height, img_width, _ = img_emotion.shape
 
         faces = face_cascade.detectMultiScale(frame_gray)
 
@@ -34,7 +92,7 @@ def generate_face(video):
                 center[1]), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
 
             try:
-                image[y+h:y+h+img_height, center[0]:center[0]+img_width] = img_emotion
+                image[y+h:y+h+img_height, center[0]                      :center[0]+img_width] = img_emotion
             except:
                 pass
 
